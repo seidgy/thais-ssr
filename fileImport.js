3604944132bcd1e6f7c6458c6ba791df052d8815
Seidgy Amano <seidgy@gmail.com>
2021-10-09 21:00:48 -0300

    fileImport fix

--- fileImport.js
+++ fileImport.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 const axios = require('axios');
+var rimraf = require("rimraf");
 require('dotenv').config({path: './.env'})
 const server = process.env['VUE_APP_SERVER_URI']?process.env['VUE_APP_SERVER_URI']:'https://backend.thaisimobiliaria.com.br';
 
@@ -8,37 +9,34 @@ const generator = async () => {
     const imoveis = await axios.post(
       server+'/imoveisHec/getAllImoveisServer'
     );
-    const dir = './content/imoveis'
-    fs.readdir(dir, (err, files) => {
-      if (err) throw err;
-      for (const file of files) {
-        fs.unlink(path.join(dir, file), err => {
-          if (err) throw err;
-        });
+    const dir = './content/imoveis'    
+    fs.rm(dir, { recursive: true }, async () => {
+      if (!fs.existsSync(dir)){
+          fs.mkdirSync(dir);
       }
+      fs.access(dir, fs.constants.R_OK | fs.constants.W_OK, async err => {
+        if (err) {
+          console.log(err)
+        } else {
+          await imoveis.data.imoveis.forEach((imovel) => {
+            let i = {}
+            i.title = imovel.codigo_imovel;
+            i.codigo_imovel = imovel.codigo_imovel;
+            i.titulo = imovel.imovel.titulo_imovel;
+            i.bairro = imovel.imovel.bairro;
+            if(imovel.ofertas[0]){
+              i.tipo = imovel.ofertas[0].tipo_oferta == 1 ? 'Aluguel' : 'Compra';
+            }
+            if(imovel.fotos[0]) {
+              i.foto = imovel.fotos[0].url_arquivo;
+            }
+            fs.writeFile(dir+"/"+imovel.codigo_imovel+".json", JSON.stringify(i), function(err, result) {
+              if(err) console.log('error', err);
+            });;
+          });
+        }
+      })
     });
-    fs.access(dir, fs.constants.R_OK | fs.constants.W_OK, async err => {
-      if (err) {
-        console.log(err)
-      } else {
-        await imoveis.data.imoveis.forEach((imovel) => {
-          let i = {}
-          i.title = imovel.codigo_imovel;
-          i.codigo_imovel = imovel.codigo_imovel;
-          i.titulo = imovel.imovel.titulo_imovel;
-          i.bairro = imovel.imovel.bairro;
-          if(imovel.ofertas[0]){
-            i.tipo = imovel.ofertas[0].tipo_oferta == 1 ? 'Aluguel' : 'Compra';
-          }
-          if(imovel.fotos[0]) {
-            i.foto = imovel.fotos[0].url_arquivo;
-          }
-          fs.writeFile(dir+"/"+imovel.codigo_imovel+".json", JSON.stringify(i), function(err, result) {
-            if(err) console.log('error', err);
-          });;
-        });
-      }
-    })
 }
 
 return generator()
